969490bc9d1997e8ce05df279a45cd1654ec8216
kkpriyan97-pixel <kkpriyan97@gmail.com>
2026-09-10 12:00:46 +0400

    Fix OlympTradeClient package export

--- olymptrade_ws/__init__.py
+++ olymptrade_ws/__init__.py
@@ -1,8 +1,13 @@
 """Olymptrade websocket package bootstrap."""
+
+# Public client used by app.py.
+from .core.client import OlympTradeClient
+
 try:
     from .api.market import MarketAPI
 except Exception:
     pass
+
 try:
     import signal_engine
 except Exception:
