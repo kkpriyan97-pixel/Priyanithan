990d3768ecadb3cc60260487684a04aba012e682
kkpriyan97-pixel <kkpriyan97@gmail.com>
2026-09-11 16:30:00 +0400

    Load Candice memory integration at startup

--- sitecustomize.py
+++ sitecustomize.py
@@ -48,6 +48,10 @@ try:
 except Exception:
     pass
 try:
+    import candice_memory_integration_hotfix  # noqa: F401
+except Exception:
+    pass
+try:
     import signal_window_countdown_hotfix  # noqa: F401
 except Exception:
     pass
