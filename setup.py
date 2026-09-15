fdfb3afcdc4fbf57951b4da0e190a3123b867e77
kkpriyan97-pixel <kkpriyan97@gmail.com>
2026-09-15 14:07:40 +0400

    Install Candice freshness boot hook

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name="candice-runtime-hooks",
-    version="1.0.0",
-    py_modules=["usercustomize"],
+    version="1.0.1",
+    py_modules=["usercustomize", "freshness_patch"],
+    data_files=[("", ["candice_boot.pth"])],
 )
