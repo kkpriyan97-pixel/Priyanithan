6f6c364f4f59751b97c4a9c1d3eb11440042930f
kkpriyan97-pixel <kkpriyan97@gmail.com>
2026-09-16 08:26:50 +0400

    Install delivery freshness hook module

--- setup.py
+++ setup.py
@@ -7,7 +7,16 @@ site_packages = os.path.relpath(sysconfig.get_path("purelib"), sys.prefix)
 
 setup(
     name="candice-runtime-hooks",
-    version="1.0.8",
-    py_modules=["usercustomize", "freshness_patch", "telegram_token_only", "outcome_recovery_guard", "account_telemetry", "account_selection_overlay", "telegram_response_patch"],
+    version="1.0.9",
+    py_modules=[
+        "usercustomize",
+        "freshness_patch",
+        "telegram_token_only",
+        "outcome_recovery_guard",
+        "account_telemetry",
+        "account_selection_overlay",
+        "telegram_response_patch",
+        "freshness_delivery_fix",
+    ],
     data_files=[(site_packages, ["candice_boot.pth"])],
 )
