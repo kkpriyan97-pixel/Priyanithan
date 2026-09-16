3992c6418cce376c5fd8bf35fd8e313e2f896257
kkpriyan97-pixel <kkpriyan97@gmail.com>
2026-09-16 10:51:24 +0400

    Reset repository to empty before clean rebuild

--- olymptrade_ws/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Read-only OlympTrade market-data connector used by Candice v8."""
-from .core.client import OlympTradeClient
-try:
-    from .api.market import MarketAPI
-except Exception:
-    MarketAPI = None
-__all__ = ['OlympTradeClient', 'MarketAPI']
